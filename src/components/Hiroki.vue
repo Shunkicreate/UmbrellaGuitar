f9e2373360779e8099abe7518db0a504353ef2b8
hirokiwa <hirokifsys@gmail.com>
2022-03-05 14:24:41 +0900

    make components

---
+++ src/components/Hiroki.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+this is Hiroki page
+<div>{{counter}}</div>
+<div><button @click="counter++">increase</button></div>
+<div><button @click="counter--">decrease</button></div>
+</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+
+
+</script>
