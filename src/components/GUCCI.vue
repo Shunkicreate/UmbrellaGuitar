360d1473704d9383841533109f42215e7eb551f0
goooochi <yuki.yamaguchi@ksf.biglobe.ne.jp>
2022-04-08 19:05:39 +0900

    メソッドを２つ追加

--- src/components/GUCCI.vue
+++ src/components/GUCCI.vue
@@ -2,8 +2,19 @@
   <div class="container">
     <div class="row">
     <div class="col-6">
+    
+      <div class="btn-group list-group-item" role="group">
+            <button class="btn btn-secondary" @click="replace">List1の順番をもとに戻す</button>
+          </div>
+
+          <div class="btn-group list-group-item" role="group">
+            <button class="btn btn-secondary" @click="reset">List2を削除</button>
+          </div>
+
       <h3 align="center">List1</h3>
+       
       <draggable class="list-group" :list="list1" :group="{name: 'people',  pull: 'clone',put: false}" @change="log">
+
         <div
           class="list-group-item"
           v-for="(element) in list1"
@@ -35,6 +46,7 @@
 <script>
   import { defineComponent } from 'vue'
   import { VueDraggableNext } from 'vue-draggable-next'
+  let id = 1;
   export default defineComponent({
     name: "GUCCI",
     display: "GUCCI",
@@ -51,18 +63,29 @@
         { name: "C", id: 3 },
         { name: "D", id: 4 },
         { name: "E", id: 5 },
-        { name: "F", id: 6 },
+        { name: "F", id: 6 }
       ],
       list2: [
-        // { name: "G", id: 7 },
+        //空
       ],
         dragging: false,
       };
     },
     methods: {
+      replace: function() {
+      this.list1 = [{ name: "A", id: 1 },
+        { name: "B", id: 2 },
+        { name: "C", id: 3 },
+        { name: "D", id: 4 },
+        { name: "E", id: 5 },
+        { name: "F", id: 6 }];
+    },
+    reset: function(){
+      this.list2 = []
+    },
       log: function(evt) {
       window.console.log(evt);
     }
-    },
-  })
+    }
+  });
 </script>
