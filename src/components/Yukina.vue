8857c586eb703c3dcb1e82a4d20b8699f77d5c0a
maruhashiyukina <yuki@LAPTOP-18NBADOJ.localdomain>
2022-04-12 13:03:32 +0900

    sa

--- src/components/Yukina.vue
+++ src/components/Yukina.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="counter">
+        <button type="button" @click="count++">count is: {{ count }}</button>
         <div class="number">
             <h5>175</h5>
         </div>
@@ -11,6 +12,9 @@
         </div>
     </div>
 
+    <div id="disp_count">0</div>
+    <input type="button" value="カウントアップ" id="btn_count_up" />
+    
     <div class="mode">
         <div class="easy">
             <p><button>簡単</button></p>
@@ -25,6 +29,13 @@
     </div>
 
 </template>
+<script lang="ts">
+import { ref } from 'vue'
+
+
+const count = ref(0)
+</script>
+
 
 <style>
     .number {
